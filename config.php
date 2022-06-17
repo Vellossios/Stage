f6362a4391bf029c8e1bf9be46dbe2c8f73b606a
Vellossios <vellossios17@gmail.com>
2022-06-17 10:05:48 +0200

    database

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
 function databaseConnect() 
 {
     /*connection a la base de donnée*/
-    return new mysqli("localhost", "root", "", "test");
+    return new mysqli("remotemysql.com", "70cybBTkrL", "PYYDGcJ3IC", "70cybBTkrL");
 } 
